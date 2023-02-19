4ab2168cc3bd7f3bf0979ca31aa069f19dad597d
Anes Smajić <anes.smajic95@hotmail.com>
2023-02-19 22:46:05 +0100

    feat: coming soon

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ContactComponent,
+    EducationComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule,
