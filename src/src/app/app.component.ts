b4cd976f85d505698ef07d6c7d937be1edf480ff
Anes Smajić <anes.smajic95@hotmail.com>
2023-09-24 10:47:50 +0200

    feat: add responsivnes

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  menuIcon?: HTMLAnchorElement;
+  navbar?: HTMLAnchorElement;
+
+  sections: HTMLAnchorElement[] = [];
+  navLinks: HTMLAnchorElement[] = [];
+
+  constructor(private elRef: ElementRef) { }
+
+  ngOnInit() {
+    this.menuIcon = this.elRef.nativeElement.querySelector('#menu-icon');
+    this.navbar = this.elRef.nativeElement.querySelector('.navbar');
+
+    this.sections = this.elRef.nativeElement.querySelectorAll('section');
+    this.navLinks = this.elRef.nativeElement.querySelectorAll('header nav a');
+  }
+
+  openMenu() {
+    this.menuIcon!.classList.toggle('bx-x');
+    this.navbar!.classList.toggle('active');
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+
+    this.sections.forEach(section => {
+      let offset = section.offsetTop - 100;
+      let height = section.offsetHeight;
+      let id = section.getAttribute('id');
+
+      if (window.scrollY >= offset && window.scrollY < offset + height) {
+
+        this.navLinks.forEach(link => {
+          link.classList.remove('active');
+
+          this.elRef
+            .nativeElement
+            .querySelector('header nav a[href*=' + id + ']')
+            .classList
+            .add('active');
+        });
+
+        section.classList.add('show-animate');
+      }
+
+    });
+
+    let header = this.elRef.nativeElement.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    this.menuIcon!.classList.remove('bx-x');
+    this.navbar!.classList.remove('active');
+  }
 }
